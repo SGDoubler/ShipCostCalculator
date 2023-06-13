4f11d08b3d0009fe04878dfe75644d95a8178f75
SGDoubler <doublesn@mail.uc.edu>
2023-06-12 21:01:17 -0400

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("How much is your item: $");
+        double itemPrice = in.nextDouble();
+        double shippingCost = itemPrice * .02;
+        double totalCost = itemPrice + shippingCost;
+
+        if (itemPrice >= 100)
+        {
+            System.out.println("Shipping is free, your total cost is: $" + itemPrice);
+        }
+        else
+        {
+            System.out.println("Shipping is: $" + shippingCost);
+            System.out.println("Your total cost is $" + totalCost);
+        }
     }
 }
